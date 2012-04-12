54fff4f3fd210737eee7a4c306ba9b400eab7db3
Leonidas Esteban <leonidasesteban@gmail.com>
2012-04-12 02:13:33 -0500

    site2

---
+++ transiciones.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	if(chrome){
+	$('#comida article:eq(0)').css('background','rgba(100,71,37,0.9)');
+	$('#comida article:eq(1)').css('background','rgba(100,71,37,0.7)');
+	$('#comida article:eq(2)').css('background','rgba(100,71,37,0.5)');
+	$('#comida article:eq(3)').css('background','rgba(100,71,37,0.3)');}
+	
+	
+	
+	
+	})
